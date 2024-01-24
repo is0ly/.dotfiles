1d4671bdfed4d565acd5fd523320c92415650755
is0ly <iljaosipov@gmail.com>
2024-01-25 00:25:15 +0300

    edited something

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 ln -sf $HOME/.dotfiles/hypr $HOME/.config/
 ln -sf $HOME/.dotfiles/kitty $HOME/.config
 ln -sf $HOME/.dotfiles/rofi $HOME/.config
+ln -sf $HOME/.dotfiles/nvim $HOME/.config
 ln -sf $HOME/.dotfiles/.gtkrc-2.0 $HOME/
+
